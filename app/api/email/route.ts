f22d862fa1bc4492797afd1b7c90e36fac9aa89e
Peter-John Hein <petej2000@gmail.com>
2023-06-30 07:30:23 +0400

    sanity integrated

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,15 +1,17 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Inputs } from "@/typings";
 
-// export async function GET(req: NextRequest) {
-// 	const { searchParams } = new URL(req.url);
-// 	const mysearchparam = searchParams.get("mysearchparam");
+export async function GET(req: NextRequest) {
+	const { searchParams } = new URL(req.url);
+	const mysearchparam = searchParams.get("mysearchparam");
 
-// 	return new Response(JSON.stringify({ message: "Hello World" }), {
-// 		headers: { "content-type": "application/json" },
-// 		status: 200,
-// 	});
-// }
+	return new Response(JSON.stringify({ message: "Hello World" }), {
+		headers: { "content-type": "application/json" },
+		status: 200,
+	});
+
+	return NextResponse.json({ message: "Hello World" });
+}
 export async function POST(req: NextRequest) {
 	const body: Inputs = await req.json();
 	const msg = {
