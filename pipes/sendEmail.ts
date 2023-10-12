d7ec7b54423e392409347342ed38f3d97da2d49a
Peter-John Hein <petej2000@gmail.com>
2023-10-13 03:37:00 +0400

    api fixes

--- pipes/sendEmail.ts
+++ pipes/sendEmail.ts
@@ -3,14 +3,11 @@ import { Inputs } from '@/typings';
 export default async function sendEmail(msg: Inputs) {
 	console.log(msg);
 	try {
-		const response = await fetch(
-			process.env.NEXT_PUBLIC_BASE_URL + '/api/email',
-			{
-				mode: 'no-cors',
-				method: 'POST',
-				body: JSON.stringify(msg),
-			}
-		);
+		const response = await fetch('/api/email', {
+			mode: 'no-cors',
+			method: 'POST',
+			body: JSON.stringify(msg),
+		});
 
 		return await response.json();
 	} catch (error) {
