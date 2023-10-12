708b9c59d18e2b1fa8d098575aeb5b9155019e87
Peter-John Hein <petej2000@gmail.com>
2023-10-13 03:31:18 +0400

    api fixes

--- pipes/sendEmail.ts
+++ pipes/sendEmail.ts
@@ -1,18 +1,20 @@
-import { Inputs } from "@/typings";
+import { Inputs } from '@/typings';
 
 export default async function sendEmail(msg: Inputs) {
+	console.log(msg);
 	try {
 		const response = await fetch(
-			process.env.NEXT_PUBLIC_BASE_URL + "/api/email",
+			process.env.NEXT_PUBLIC_BASE_URL + '/api/email',
 			{
-				method: "POST",
+				mode: 'no-cors',
+				method: 'POST',
 				body: JSON.stringify(msg),
 			}
 		);
 
 		return await response.json();
 	} catch (error) {
-		console.log("There was an error submitting", error);
-		return { message: "There was an error submitting" };
+		console.log('There was an error submitting', error);
+		return { message: 'There was an error submitting' };
 	}
 }
